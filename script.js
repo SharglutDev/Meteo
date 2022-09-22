6324ff4a7b966682fd9fc70e178c2a954ed86b96
sharglut <sharglut@free.fr>
2022-09-22 14:29:46 +0200

    v1

---
+++ script.js
@@ -0,0 +1,58 @@
+const container = document.querySelector(".container");
+const input = document.querySelector("#city-input");
+const fetchMeteoBtn = document.querySelector(".meteo-btn");
+
+const result = document.querySelector("#result");
+const city = document.querySelector(".city");
+const weather = document.querySelector(".weather");
+const weatherIcon = document.querySelector(".weatherImg");
+const temp = document.querySelector(".tmp");
+const error = document.querySelector(".error");
+
+// ******* FETCH METEO ********
+
+const fetchMeteo = async () => {
+  let cityToFetch = input.value;
+
+  const response = await fetch(
+    `https://www.prevision-meteo.ch/services/json/${cityToFetch}`
+  );
+  const data = await response.json();
+  return data;
+};
+
+// const fetchImg = (data) => {
+//   const isTHereAnImg = document.querySelector("img");
+//   if (isTHereAnImg) isTHereAnImg.remove();
+//   const img = document.createElement("img");
+//   img.src = `${data}`;
+//   weather.appendChild(img);
+// };
+
+// ******* CHANGE CITY ********
+
+fetchMeteoBtn.addEventListener("click", () => {
+  if (container.classList.contains("disabled")) {
+    container.classList.toggle("disabled");
+  }
+  fetchMeteo().then((data) => {
+    if (data.errors) {
+      error.classList.remove("disabled");
+      result.classList.add("disabled");
+    } else {
+      error.classList.add("disabled");
+      result.classList.remove("disabled");
+      city.innerHTML = `${data.city_info.name}`;
+      // fetchImg(data.current_condition.icon_big);
+      weatherIcon.src = data.current_condition.icon_big;
+      temp.innerHTML = `${data.current_condition.tmp}°C`;
+    }
+  });
+});
+
+input.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    fetchMeteoBtn.click();
+  }
+});
